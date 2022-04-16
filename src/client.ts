5c196fff7c8339553d0052b07de8d11726f87a80
HongJinHyeok <redmax45@naver.com>
2022-04-16 12:44:05 +0900

    ♻️  chat client

--- src/client.ts
+++ src/client.ts
@@ -5,6 +5,7 @@ const __dirname = path.resolve();
 import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
 import { ProtoGrpcType } from '../proto/random';
+import readline from 'readline';
 
 const PORT = 8082;
 const PROTO_FILE = './proto/random.proto';
@@ -47,16 +48,47 @@ function onClientReady() {
   //   console.log('communication ended');
   // });
   // ! TodoList
-  const stream = client.TodoList((err, result) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(result);
+  // const stream = client.TodoList((err, result) => {
+  //   if (err) {
+  //     console.error(err);
+  //     return;
+  //   }
+  //   console.log(result);
+  // });
+  // stream.write({ todo: 'walk the wife', status: 'Never' });
+  // stream.write({ todo: 'walk the dog', status: 'Doing' });
+  // stream.write({ todo: 'get a real job', status: 'Impossible' });
+  // stream.write({ todo: 'feed the dog', status: 'Done' });
+  // stream.end();
+
+  // ! Chat
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
-  stream.write({ todo: 'walk the wife', status: 'Never' });
-  stream.write({ todo: 'walk the dog', status: 'Doing' });
-  stream.write({ todo: 'get a real job', status: 'Impossible' });
-  stream.write({ todo: 'feed the dog', status: 'Done' });
-  stream.end();
+
+  const username = process.argv[2];
+  if (!username) console.error("No username, can't join chat"), process.exit();
+
+  const metadata = new grpc.Metadata();
+  metadata.set('username', username);
+  const call = client.Chat(metadata);
+
+  call.write({
+    message: 'register',
+  });
+
+  call.on('data', chunk => {
+    console.log(`${chunk.username}: ${chunk.message}`);
+  });
+
+  rl.on('line', line => {
+    if (line === 'quit') {
+      call.end();
+    } else {
+      call.write({
+        message: line,
+      });
+    }
+  });
 }
